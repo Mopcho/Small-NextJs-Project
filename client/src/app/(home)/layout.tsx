722e016fdde879bda1079a5b22edcc24bfddd0fc
Mopcho <valeri_petkov01@abv.bg>
2023-04-01 19:54:09 +0300

    Some style improvements

--- client/src/app/(home)/layout.tsx
+++ client/src/app/(home)/layout.tsx
@@ -6,9 +6,9 @@ export default function HomeLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className="m-7">
+    <section className="mx-24 my-10">
         <nav className="flex py-5 justify-between">
-            <h2 className="text-4xl">MopBlog</h2>
+            <h2 className="text-4xl select-none">MopBlog</h2>
             <ul className="flex gap-4 text-xl">
                 <Link href={'/'} className='hover:text-red-500 hover:scale-110'>Home</Link>
                 <Link href={'/about'} className='hover:text-red-500 hover:scale-110'>About</Link>
