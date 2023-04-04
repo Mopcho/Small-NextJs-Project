e27ce76711d06c4faa2afed70c19852c5cb8cfcb
Mopcho <valeri_petkov01@abv.bg>
2023-04-05 00:35:12 +0300

    Remade the navigation

--- client/src/app/(home)/layout.tsx
+++ client/src/app/(home)/layout.tsx
@@ -6,13 +6,20 @@ export default function HomeLayout({
   children: React.ReactNode
 }) {
   return (
-    <section className="mx-24 my-10">
-        <nav className="flex py-5 justify-between">
-            <h2 className="text-4xl select-none">MopBlog</h2>
-            <ul className="flex gap-4 text-xl">
+    <section className="mx-48 my-10">
+        <nav className="flex py-5 justify-end">
+            <ul className="flex gap-4 text-2xl justify-center items-center justify-items-center border-b-2 pb-4 border-white">
                 <Link href={'/'} className='hover:text-red-500 hover:scale-110'>Home</Link>
+                <p>/</p>
                 <Link href={'/about'} className='hover:text-red-500 hover:scale-110'>About</Link>
+                <p>/</p>
                 <Link href={'/blog'} className='hover:text-red-500 hover:scale-110'>Blog</Link>
+                <p>/</p>
+                <Link href={'/login'} className='hover:text-red-500 hover:scale-110'>Login</Link>
+                <p>/</p>
+                <Link href={'/register'} className='hover:text-red-500 hover:scale-110'>Register</Link>
+                <p>/</p>
+                <button className='hover:text-red-500 hover:scale-110'>More <span className="text-3xl">+</span></button>
             </ul>
         </nav>
         {children}
--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <main> This is the Home route</main>
+    <main></main>
   )
 }
