458731ceee95f8d07e6639303a2bfb85d757c042
MopchoNex <mopchowork@gmail.com>
2023-05-09 23:08:48 +0300

    Fixed some input stuff

--- client/src/app/(home)/login/page.tsx
+++ client/src/app/(home)/login/page.tsx
@@ -6,10 +6,10 @@ export default function Login() {
       <section className="flex justify-center flex-1 items-center form-section-main">
           <form className="login-form-layout">
                 <label htmlFor="email" className="flex flex-col">Email
-                <input className="login-email" placeholder="Email..." name="email"></input>
+                <input className="login-email" placeholder="Email..." name="email" type="email"></input>
                 </label>
-                <label htmlFor="email" className="flex flex-col">Password
-                <input className="login-password" placeholder="Password..." name="password"></input>
+                <label htmlFor="password" className="flex flex-col">Password
+                <input className="login-password" placeholder="Password..." name="password" type="password"></input>
                 </label>
                 <button className="login-submit">Login</button>
                 <Link href={`/${ROUTE_REGISTER}`}>Don't have an account ? Sign up now !</Link>
