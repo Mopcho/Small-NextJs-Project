2922f175e98a5e4a67383cb438475ca5f0fa6697
MopchoNex <mopchowork@gmail.com>
2023-05-09 23:34:46 +0300

    Reduce hero image size

--- client/src/app/(home)/page.tsx
+++ client/src/app/(home)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <section className="hero-layout">
       <main className='flex flex-col items-center gap-6'>
-        <img src={HOMEPAGE_IMAGE.src} alt='hero' className="aspect-square rounded-xl w-3/4"></img>
+        <img src={HOMEPAGE_IMAGE.src} alt='hero' className="aspect-square rounded-xl h-[500px]"></img>
         <h2 className='text-custom-green text-4xl break-keep text-center'>A blog worth browsing!</h2>
         <Link href={`/${ROUTE_LOGIN}`} className='text-white text-2xl bg-blue-800 py-3 px-5 rounded-full font-bold hover:scale-110'>Login Now</Link>
       </main>
