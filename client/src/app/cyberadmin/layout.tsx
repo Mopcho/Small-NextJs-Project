ea8b9e26276d810e5113670ab021e1abfa453859
MopchoNex <mopchowork@gmail.com>
2023-05-28 03:01:08 +0300

    Protect admin part

--- client/src/app/cyberadmin/layout.tsx
+++ client/src/app/cyberadmin/layout.tsx
@@ -1,10 +1,18 @@
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { redirect } from 'next/navigation';
 import Link from 'next/link';
 
-export default function CyberAdminLayout({
+export default async function CyberAdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await getServerSession(authOptions);
+
+  if (!session || session?.user?.email !== 'test@abv.bg') {
+    redirect('api/auth/signin');
+  }
   return (
     <section className="admin-layout custom-container py-5">
       <nav className="admin-navigation">
